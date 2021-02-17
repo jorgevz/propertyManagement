483171b0fb7cf24a3ac4eb5154bdf6d7fa1061f0
jorgevz <jorgev2132@gmail.com>
2021-02-16 23:09:04 -0500

    carousel up and running, however images might change

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,10 @@
 import React from 'react';
+// import {apt1} from '../images/apt1.jpg';
+// import {apt2} from '../images/apt2.jpg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import {Carousel} from 'react-bootstrap';
+import Carousel from 'react-bootstrap/Carousel'
+import { Button } from 'react-bootstrap';
 
 const Home = () => {
  
@@ -7,12 +13,66 @@ return(
     
 <div>
 
-<nav>
+<ul class="nav justify-content-center" id="nav">
 
-</nav>
+<li id='logo' class="nav-item">
+          <a class="nav-link active" href="#">#</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Submit Tickets</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Units Available</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Screening</a>
+        </li>
+      </ul>
 
+
+
+<div>
+<Carousel>
+  <Carousel.Item interval={3000}>
+    <img
+      className="d-block w-100"
+      src='https://cdn.vox-cdn.com/thumbor/MFQLpHmnFrma-qGt9zq8qJ6UU-c=/0x0:1000x668/1200x800/filters:focal(420x254:580x414)/cdn.vox-cdn.com/uploads/chorus_image/image/62947990/285_5thAve2R_Kitchen_straight_on.6.jpg'
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h3>Beautiful</h3>
+      <Button variant='info'>Inquire Today</Button>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item interval={3000}>
+    <img
+      className="d-block w-100"
+      src='https://wp-tid.zillowstatic.com/streeteasy/2/751-79959c.jpg'
+      alt="Second slide"
+    />
+    <Carousel.Caption>
+      <h3>Convenient</h3>
+      <Button variant='info'>Inquire Today</Button>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src='https://i1.wp.com/nypost.com/wp-content/uploads/sites/2/2016/06/214217130.jpg?quality=80&strip=all&ssl=1'
+      alt="Third slide"
+    />
+    <Carousel.Caption>
+      <h2>Affordable</h2>
+    <Button variant='info'>Inquire Today</Button>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+  
 
 </div>
+
+</div>
+
 
 
 
