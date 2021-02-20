885d2b1b5da373e040fddfb3efbf5f66de1abfc2
jorgevz <jorgev2132@gmail.com>
2021-02-19 21:07:48 -0500

    login page made

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,15 +7,15 @@ function Login () {
 
     return(
        <div>
-           <form>
-
+              <form id='form'>
         <input placeholder='username'/>
+        <br/>
         <input placeholder='password'/>
-        
+        <br/>
         <Button variant='primary'>Login</Button>
-       
+        <br/>
         <h6>Don't have account ? Sign Up !</h6>
-       
+        <br/>
         <Button variant='info'>Sign Up</Button>
 
            </form>
