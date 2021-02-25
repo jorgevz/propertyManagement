b55481ab3072cac65a1e1fdd43168c4c9b8efca8
jorgevz <jorgev2132@gmail.com>
2021-02-25 12:51:26 -0500

    local images accepted finally, old ones deleted

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {apt1} from '../images/apt1.jpg';
-import {apt2} from '../images/apt2.jpg';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import {Carousel} from 'react-bootstrap';
 import Carousel from 'react-bootstrap/Carousel'
 import { Button } from 'react-bootstrap';
+import apartment from '../apartment.jpg';
+import apartment1 from '../apartment1.jpg';
+import apartment2 from '../apartment2.jpg';
 
 const Home = () => {
  
@@ -14,13 +14,12 @@ return(
 <div>
 
 
-
 <div>
 <Carousel>
   <Carousel.Item interval={3000}>
     <img
       className="d-block w-100"
-      src='https://cdn.vox-cdn.com/thumbor/MFQLpHmnFrma-qGt9zq8qJ6UU-c=/0x0:1000x668/1200x800/filters:focal(420x254:580x414)/cdn.vox-cdn.com/uploads/chorus_image/image/62947990/285_5thAve2R_Kitchen_straight_on.6.jpg'
+      src={apartment}
       alt="First slide"
     />
     <Carousel.Caption>
@@ -31,7 +30,7 @@ return(
   <Carousel.Item interval={3000}>
     <img
       className="d-block w-100"
-      src='https://wp-tid.zillowstatic.com/streeteasy/2/751-79959c.jpg'
+      src={apartment1}
       alt="Second slide"
     />
     <Carousel.Caption>
@@ -42,7 +41,7 @@ return(
   <Carousel.Item>
     <img
       className="d-block w-100"
-      src='https://i1.wp.com/nypost.com/wp-content/uploads/sites/2/2016/06/214217130.jpg?quality=80&strip=all&ssl=1'
+      src={apartment2}
       alt="Third slide"
     />
     <Carousel.Caption>
